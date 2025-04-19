7d16382073da01c2865ef68fc2d31e88f8bf3d28
akash6354 <145893079+akash6354@users.noreply.github.com>
2025-04-19 21:31:55 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+
+
+
+
+client = MongoClient("mongodb+srv://proversionofakash:yXmPmDDhLvZfSx62@cluster0.eiefq2m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client.confessionDB
+confessions = db.confessions
